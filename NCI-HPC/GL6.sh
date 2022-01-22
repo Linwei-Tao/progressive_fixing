1d325bb445260f63851d545e34ecf5c14d8bc387
dylan <dylan.tao94@gmail.com>
2022-01-23 00:15:24 +1100

    hpc tasks

---
+++ NCI-HPC/GL6.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#PBS -l ncpus=12
+#PBS -l mem=30GB
+#PBS -l jobfs=200GB
+#PBS -l ngpus=1
+#PBS -q gpuvolta
+#PBS -P li96
+#PBS -l walltime=10:00:00
+#PBS -l storage=gdata/li96+scratch/li96
+#PBS -l wd
+
+filename="GL6"
+modelname="resnet50"
+dataset="cifar10"
+loss="cross_entropy"
+PF_criterion="GL"
+alpha=6
+projectDIR="progressive_fixing"
+
+module load python3/3.9.2
+module load pytorch/1.9.0
+cd /scratch/$PROJECT/$USER/$projectDIR
+python3 train.py --dataset=$dataset --model=$modelname --loss=$loss --PF_criterion=$PF_criterion --UP_alpha=$alpha >/scratch/$PROJECT/$USER/progressive_fixing/NCI-HPC/logs/$filename.log
